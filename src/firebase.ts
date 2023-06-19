715e25babe16ee8623c6a3d1237898cc37b53641
Nathan Asowata <nathanasowata@gmail.com>
2023-06-19 10:25:14 +0100

    chore: setup app

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.API_KEY,
+  authDomain: "astrotech-d9866.firebaseapp.com",
+  projectId: "astrotech-d9866",
+  storageBucket: "astrotech-d9866.appspot.com",
+  messagingSenderId: "1031811524383",
+  appId: "1:1031811524383:web:3eab8c97eab769060d722a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
