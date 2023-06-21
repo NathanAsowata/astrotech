a534ec8fbf08205fb5d22f043081a1c8f9822290
Nathan Asowata <nathanasowata@gmail.com>
2023-06-21 09:14:05 +0100

    style: navbar skeleton for large screens completed

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,34 +1,57 @@
-import { Badge, Box, Grid, Typography } from "@mui/material"
-import logo from "../assets/astrotech.png"
-import { AccountCircle, ShoppingCart } from "@mui/icons-material"
-
+import logo from "../assets/astrotech.png";
+import { Badge, Grid, Typography } from "@mui/material";
+import { AccountCircle, ShoppingCart } from "@mui/icons-material";
 
 function NavBar() {
   return (
-    <Grid container spacing={2} py={2} px={5} >
-        <Grid item sm={2}>
+    <Grid
+      container
+      spacing={2}
+      p={1}
+      alignItems="center"
+      sx={{
+        borderBottom: "1px solid #4169e1",
+        boxShadow: "0px 3px 6px #1c1c1c",
+      }}
+    >
+      <Grid item sm={2.5}>
         <img src={logo} alt="Astrotech Logo" width={38} />
-        <Typography 
-            variant="h4" 
-            color="primary" 
-            sx={{
-                display: "inline-block", 
-                fontWeight: "600"
-            }}
+        <Typography
+          variant="h4"
+          color="primary"
+          sx={{
+            display: "inline-block",
+            fontWeight: "600",
+          }}
         >
-        strotech
+          strotech
         </Typography>
-        </Grid>
+      </Grid>
 
-        <Box>
-            <Badge badgeContent={5} color="secondary">
-                <ShoppingCart />
-            </Badge>
-            
-            <AccountCircle />
-        </Box>
+      <Grid item sm={7} textAlign={"center"}>
+        <Typography variant="body1">SearchBar comes here</Typography>
+      </Grid>
+
+      <Grid item sm={2.5} sx={{ display: "flex", alignItems: "center" }}>
+        <Badge badgeContent={5} color="primary">
+          <ShoppingCart color="secondary" fontSize="large" />
+        </Badge>
+
+        <AccountCircle
+          color="secondary"
+          fontSize="large"
+          sx={{ marginLeft: 3 }}
+        />
+        <Typography
+          variant="body1"
+          fontWeight={"bold"}
+          sx={{ alignSelf: "end" }}
+        >
+          Hello, Nathan
+        </Typography>
+      </Grid>
     </Grid>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
